0666245b515839a9dcf512165b4fbb7d7f36f09e
JEROME T. DE GUZMAN <deguzmjj@ph.ibm.com>
2017-11-07 01:28:40 +0800

    PUM-004 (#3)
    * Added page for reset password link
    Bug fix for successful login using an Admin account
    
    * Added call to token validation endpoint in resetPassword method for reset password link validity
    Added logging support

---
+++ WebContent/pages/admin/resetPassword.jsp
@@ -0,0 +1,30 @@
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<link rel="stylesheet" type="text/css" href="../resources/system/css/resetPasswordStyle.css" />
+
+<s:head/>
+<div class="formResetPasswordStyle">
+	<form action="resetPasswordLink" method="post" enctype="application/x-www-form-urlencoded">
+		<h1>Reset Password</h1>
+		<div class="inset">
+			<p>
+				<br>
+				<s:if test="hasActionErrors()">
+			<%-- 	<div style="margin-left: 10px;">
+						<s:actionerror cssStyle="list-style: none;"/>
+				</div> --%>
+				</s:if>
+				<label for="oldPassword">Old Password: </label>
+				<input type="password" name="oldPassword" required="required" width= "248px" />
+				<label for="newPassword">New Password: </label>
+				<input type="password" name="newPassword" required="required" width= "248px" />
+				<label for="confirmNewPassword">Confirm New Password: </label>
+				<input type="password" name="confirmNewPassword" required="required" width= "248px" />
+				<s:submit name="submit" key="Submit" onclick="getName()" />
+
+				<br>
+				<br>
+				
+			</p>
+		</div>
+	</form>
+</div>
--- admin/com/ibm/opum/admin/action/AdminAction.java
+++ admin/com/ibm/opum/admin/action/AdminAction.java
@@ -5,13 +5,23 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import com.ibm.opum.admin.bean.JavaToJsonUtil;
 import com.ibm.opum.admin.bean.JsonToJavaUtil;
 import com.ibm.opum.admin.bean.PUMYear;
 import com.ibm.opum.admin.bean.PUMYearList;
+import com.ibm.opum.user.bean.ResetPassword;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
 public class AdminAction extends ActionSupport {
+
+	private Logger logger = Logger.getLogger(AdminAction.class);
 
 	private static final long serialVersionUID = 1L;
 	private PUMYearList pumYearList;
@@ -19,6 +29,7 @@ public class AdminAction extends ActionSupport {
 	
 	public String home(){
 		ActionContext.getContext().getSession().put("role", "admin");
+		
 		return "adminHome";
 	}
 	
@@ -47,30 +58,29 @@ public class AdminAction extends ActionSupport {
 		String jsonData = null;
 		
 		try {
-			URL url = new URL("http://localhost:9090/onlinePUM/webapi/opum/yearList");
+			URL url = new URL("http://localhost:8080/online-pum-rest/webapi/opum/yearList");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Accept", "application/json");
 			connection.setRequestProperty("username", ActionContext.getContext().getSession().get("username") +"");
 			connection.setRequestProperty("password", ActionContext.getContext().getSession().get("password") +"");
-			System.out.println("Hello");
 			if (connection.getResponseCode() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
 			}
 			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
 			while ((jsonData = in.readLine()) != null) {
-				System.out.println("Jason Data: " + jsonData);
+				logger.info("Jason Data: " + jsonData);
 				pumYearList = JsonToJavaUtil.JsonToJava(jsonData, PUMYearList.class);
 
 			}
 			for(PUMYear pumyear: pumYearList.getPumYearList()){
-					System.out.print("PUMYear:"+pumyear.getPumYear());
-					System.out.print("Start Date: "+pumyear.getStart());
-					System.out.print("End Date:"+pumyear.getEnd());
+				logger.info("PUMYear:"+pumyear.getPumYear());
+				logger.info("Start Date: "+pumyear.getStart());
+				logger.info("End Date:"+pumyear.getEnd());
 			}
 
-			System.out.println("\nCrunchify REST Service Invoked Successfully..");
+			logger.info("\nCrunchify REST Service Invoked Successfully..");
 			in.close();
 			connection.disconnect();
 		} catch (Exception e) {
@@ -106,6 +116,22 @@ public class AdminAction extends ActionSupport {
 		return "searchHolidayLink";
 	}
 
+	public String resetPassword(){
+		ResetPassword resetPassword = new ResetPassword();
+		resetPassword.setEmail(ServletActionContext.getRequest().getParameter("email"));
+		resetPassword.setToken(ServletActionContext.getRequest().getParameter("token"));
+		
+		Client client = Client.create();
+		WebResource webResource = client.resource("http://localhost:8080/online-pum-rest/webapi/resetPassword/validateToken");
+		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, JavaToJsonUtil.javaToJson(resetPassword));
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+			     + response.getStatus());
+		}
+		return "resetPasswordLink";
+	}
+	
 	public PUMYearList getPumYearList() {
 		return pumYearList;
 	}
@@ -121,7 +147,5 @@ public class AdminAction extends ActionSupport {
 	public void setYear(String year) {
 		this.year = year;
 	}
-	
-	
 
 }
