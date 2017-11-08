3c939a0c246de4f26cfb4d3a3cc1db54fe4880e3
JEROME T. DE GUZMAN <deguzmjj@ph.ibm.com>
2017-11-08 22:28:46 +0800

    added ResetPassword entity (#5)

---
+++ user/com/ibm/opum/user/bean/ResetPassword.java
@@ -0,0 +1,24 @@
+package com.ibm.opum.user.bean;
+
+public class ResetPassword {
+
+	private String email;
+	private String token;
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+}
